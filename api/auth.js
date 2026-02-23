fef214ba23f4c48ce41df7af7c5a94b011305e32
yassine060793 <yassinekahou@gmail.com>
2026-02-23 02:12:06 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,42 +1,5 @@
-export default async function handler(req, res) {
-  const { code } = req.query;
-  const client_id = process.env.OAUTH_CLIENT_ID;
-  const client_secret = process.env.OAUTH_CLIENT_SECRET;
-
-  const response = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    },
-    body: JSON.stringify({
-      client_id,
-      client_secret,
-      code,
-    }),
-  });
-
-  const result = await response.json();
-  
-  if (result.error) {
-    res.status(401).send(`Error: ${result.error_description}`);
-  } else {
-    res.send(`
-      <script>
-        const receiveMessage = (message) => {
-          window.opener.postMessage(
-            'authorization:github:success:${JSON.stringify({
-              token: result.access_token,
-              provider: 'github',
-            })}',
-            message.origin
-          );
-          window.removeEventListener('message', receiveMessage, false);
-        }
-        window.addEventListener('message', receiveMessage, false);
-        window.opener.postMessage("authorizing:github", "*");
-      </script>
-    `);
-  }
+// /api/auth.js
+export default function handler(req, res) {
+  const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
+  res.redirect(redirect);
 }
-
