56e7114967fdb781dc2937f8d82e4c21659f98d6
yassine060793 <yassinekahou@gmail.com>
2026-02-24 03:26:05 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const client_id = process.env.GITHUB_CLIENT_ID;
-  const redirect_uri = "https://nyk-construction.vercel.app/api/callback"; // رابطك الفعلي
+  const redirect_uri = "https://nyk-construction.vercel.app/api/callback"; // الرابط الكامل
   const githubAuthURL = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=repo`;
   res.redirect(githubAuthURL);
 }
